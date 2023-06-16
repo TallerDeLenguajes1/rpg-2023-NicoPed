fa3607ffed6a759aba59133ce9dc642581dad468
NicoPed <nikopedraza10@gmail.com>
2023-06-16 15:47:56 -0300

    según yo terminado fabrica de personajes(aún debe probarse)

--- fabricaDePersonaje.cs
+++ fabricaDePersonaje.cs
@@ -2,9 +2,9 @@ namespace Personajes;
 
 public class fabricaDePersonaje{
 private string [ , ] nombresDemonios = {{"Enmu","Luna Inferior Uno"},{"Rokuro","Luna Inferior Dos"},{"Wakuraba","Luna Inferior Tres"},{"Mukago","Luna Inferior Cuatro"},{"Rui","Luna Inferior Tres"}};
-private string [ , ] nombresCrecientes = {{"Creciente 1","xd"},{"Tanjiro","Kamado"}};
-private string [ , ] nombresCazadores = {{"Creciente 1","xd"},{"Tanjiro","Kamado"}};
-private string [ , ] nombresHashiras = {{"Creciente 1","xd"},{"Tanjiro","Kamado"}};
+private string [ , ] nombresCrecientes = {{"Gyokko","Luna Superior Cinco"},{"Nakime","Luna Superior Cuatro"},{"Akaza","Luna Superior Tres"},{"Doma","Luna Superior Dos"},{"Kokushibo","Luna Superior Uno"}};
+private string [ , ] nombresHashira = {{"Zenitsu","Respiración del Rayo"},{"Tanjiro","Respiración Solar"},{"Inosuke","Respiración de la Bestia"},{"Kanao","Respiración De La Mariposa"},{"Genya","Come Demonios"}};
+private string [ , ] nombresHashiras = {{"Tomioka","Respiración del Agua"},{"Uzui","Respiración del Sonido"},{"Shinobu","Respiración de la Mariposa"},{"Muichiro","Respiración De La neblina"},{"Mitsuri","Respiración Del Amor"}};
 
     public Personaje crearPersonaje(){
         var nuevoPersonaje = new Personaje();
@@ -19,12 +19,13 @@ private string [ , ] nombresHashiras = {{"Creciente 1","xd"},{"Tanjiro","Kamado"
                 nuevoPersonaje = crearDemonio();
             break;
             case tipoDePersonaje.creciente:
+                nuevoPersonaje = crearCreciente();
             break;
             case tipoDePersonaje.cazador:
                 nuevoPersonaje =crearCazador();
             break;
             case tipoDePersonaje.hashira:
-
+                nuevoPersonaje = crearHashira();
             break;
         }
         return nuevoPersonaje;
@@ -63,8 +64,8 @@ private string [ , ] nombresHashiras = {{"Creciente 1","xd"},{"Tanjiro","Kamado"
         DateTime fechaDeNacimiento;
         
         nuevoCazador.Tipo = tipoDePersonaje.cazador;
-        nuevoCazador.Nombre = nombresCazadores[numeroRandom,0];
-        nuevoCazador.Apodo = nombresCazadores[numeroRandom,1];
+        nuevoCazador.Nombre = nombresHashira[numeroRandom,0];
+        nuevoCazador.Apodo = nombresHashira[numeroRandom,1];
         if (DateTime.TryParse(stringFecha,out fechaDeNacimiento))
         {
             nuevoCazador.Fecha_nac = fechaDeNacimiento;
@@ -82,30 +83,56 @@ private string [ , ] nombresHashiras = {{"Creciente 1","xd"},{"Tanjiro","Kamado"
         return nuevoCazador;
     }
     private Personaje crearHashira(){
-        var nuevoCazador = new Personaje();
+        var nuevoHashira = new Personaje();
         var random = new Random();
         var numeroRandom = random.Next(0,5);
         string stringFecha = random.Next(1,28)+"/"+random.Next(1,12)+"/"+random.Next(1980,2010);
         DateTime fechaDeNacimiento;
         
-        nuevoCazador.Tipo = tipoDePersonaje.cazador;
-        nuevoCazador.Nombre = nombresCazadores[numeroRandom,0];
-        nuevoCazador.Apodo = nombresCazadores[numeroRandom,1];
+        nuevoHashira.Tipo = tipoDePersonaje.hashira;
+        nuevoHashira.Nombre = nombresHashira[numeroRandom,0];
+        nuevoHashira.Apodo = nombresHashira[numeroRandom,1];
         if (DateTime.TryParse(stringFecha,out fechaDeNacimiento))
         {
-            nuevoCazador.Fecha_nac = fechaDeNacimiento;
+            nuevoHashira.Fecha_nac = fechaDeNacimiento;
         }else
         {
-            nuevoCazador.Fecha_nac = new DateTime(1980,12,5);
+            nuevoHashira.Fecha_nac = new DateTime(1980,12,5);
         }
-        nuevoCazador.Edad = nuevoCazador.SacarEdad();
-        nuevoCazador.Velocidad = random.Next(1,5);
-        nuevoCazador.Destreza = random.Next(1,3);
-        nuevoCazador.Fuerza = random.Next(1,5);
-        nuevoCazador.Nivel = random.Next(1,5);
-        nuevoCazador.Defensa = random.Next(1,5);
-        nuevoCazador.Salud = 100;
-        return nuevoCazador;
+        nuevoHashira.Edad = nuevoHashira.SacarEdad();
+        nuevoHashira.Velocidad = random.Next(5,10);
+        nuevoHashira.Destreza = random.Next(3,5);
+        nuevoHashira.Fuerza = random.Next(5,10);
+        nuevoHashira.Nivel = random.Next(5,10);
+        nuevoHashira.Defensa = random.Next(5,10);
+        nuevoHashira.Salud = 100;
+        return nuevoHashira;
+    }
+        private Personaje crearCreciente(){
+        var nuevoCreciente = new Personaje();
+        var random = new Random();
+        var numeroRandom = random.Next(0,5);
+        string stringFecha = random.Next(1,28)+"/"+random.Next(1,12)+"/"+random.Next(1000,2000);
+        DateTime fechaDeNacimiento;
+        
+        nuevoCreciente.Tipo = tipoDePersonaje.creciente;
+        nuevoCreciente.Nombre = nombresDemonios[numeroRandom,0];
+        nuevoCreciente.Apodo = nombresDemonios[numeroRandom,1];
+        if (DateTime.TryParse(stringFecha,out fechaDeNacimiento))
+        {
+            nuevoCreciente.Fecha_nac = fechaDeNacimiento;
+        }else
+        {
+            nuevoCreciente.Fecha_nac = new DateTime(1000,12,5);
+        }
+        nuevoCreciente.Edad = nuevoCreciente.SacarEdad();
+        nuevoCreciente.Velocidad = random.Next(6,11);
+        nuevoCreciente.Destreza = random.Next(3,5);
+        nuevoCreciente.Fuerza = random.Next(6,11);
+        nuevoCreciente.Nivel = random.Next(6,11);
+        nuevoCreciente.Defensa = random.Next(6,11);
+        nuevoCreciente.Salud = 100;
+        return nuevoCreciente;
     }
 }
 // 
