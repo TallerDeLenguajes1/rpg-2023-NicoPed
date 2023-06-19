7ee594a2b4102b35454e63b617bc45b6c44985df
NicoPed <nikopedraza10@gmail.com>
2023-06-19 13:35:54 -0300

    Los combates funcionan bien, debo mejorar los stats

--- Program.cs
+++ Program.cs
@@ -23,8 +23,16 @@ internal class Program
         {
             listaDePersonajes =AyudaJson.LeerPersonajes(archivoJson);
         }    
-            mostrarLista(listaDePersonajes);
-            
+        // mostrarLista(listaDePersonajes);
+        Personaje perdedor;
+        perdedor = combateJugador(listaDePersonajes[1],listaDePersonajes[1]);
+        Console.WriteLine("============================");
+        Console.WriteLine("============================");
+        Console.WriteLine("Perdedor: ");
+        mostrarDatos(perdedor);
+        Console.WriteLine("============================");
+        Console.WriteLine("============================");
+        listaDePersonajes.Remove(perdedor);
     }
 
     private static void mostrarLista(List<Personaje> listaDePersonajes){
@@ -55,6 +63,10 @@ internal class Program
         var random = new Random();
         while (luchador1.Salud >0 && luchador2.Salud >0)
         {
+            Console.WriteLine("============================");
+            Console.WriteLine("Salud P1:" + luchador1.Salud);
+            Console.WriteLine("Salud P2:" + luchador2.Salud);
+            Console.WriteLine("============================");
             moneda = random.Next(1,3);
             if (moneda == 1)
             {
@@ -72,10 +84,12 @@ internal class Program
             }
         }
         if (luchador1.Salud <= 0 ){
+            luchador2.Salud = 100;
             return luchador1;
         }
         else
         {
+            luchador1.Salud = 100;
             return luchador2;
         }
 
@@ -83,19 +97,132 @@ internal class Program
     private static int calcularGolpe(Personaje luchador, int fuerzaExtra){
         var random = new Random();
         int danioProvocado = 0;
-        int Ataque = luchador.Destreza * luchador.Fuerza + fuerzaExtra * luchador.Nivel;
+        int Ataque = luchador.Destreza * (luchador.Fuerza + fuerzaExtra) * luchador.Nivel;
         int Efectividad = random.Next(0,101);
         int Defensa = luchador.Defensa * luchador.Velocidad;
         int ConstanteDeAjuste = 500; 
         danioProvocado = ((Ataque * Efectividad)- Defensa)/ConstanteDeAjuste;
         return danioProvocado;
     }
-    private static Personaje combateJugador(Personaje luchador1, Personaje luchador2){
-        while (luchador1.Salud >0 && luchador2.Salud >0)
+    private static Personaje combateJugador(Personaje PersonajePrincipal, Personaje Contrincante){
+        while (PersonajePrincipal.Salud >0 && Contrincante.Salud >0)
         {
-            
+        int golpePersonajePrincipal;
+        int golpeContrincante;
+        int Resultado;
+        while (PersonajePrincipal.Salud >0 && Contrincante.Salud >0)
+        {
+            Console.WriteLine("============================");
+            Console.WriteLine("Tu salud:" + PersonajePrincipal.Salud);
+            Console.WriteLine("Contrincante:" + Contrincante.Salud);
+            Console.WriteLine("============================");
+            Resultado = elMasCercano();
+            if (Resultado == 3)
+            {
+                Console.WriteLine("===Empate!!==");
+                golpePersonajePrincipal = calcularGolpe(PersonajePrincipal,0);
+                golpeContrincante = calcularGolpe(Contrincante,0); 
+            }else
+            {    
+                if (Resultado == 1)
+                {
+                    Console.WriteLine("==Tu ganas==");
+                    golpePersonajePrincipal = calcularGolpe(PersonajePrincipal,10);
+                    golpeContrincante = calcularGolpe(Contrincante,0);
+                }else
+                {
+                    Console.WriteLine("==Pierdes==");
+                    golpePersonajePrincipal = calcularGolpe(PersonajePrincipal,0);
+                    golpeContrincante = calcularGolpe(Contrincante,10);              
+                }
+            }
+            Contrincante.Salud -= golpePersonajePrincipal;
+            if (Contrincante.Salud > 0)
+            {
+                PersonajePrincipal.Salud -= golpeContrincante;
+            }
         }
-        return luchador1;
+        if (PersonajePrincipal.Salud <= 0 ){
+            return PersonajePrincipal;
+        }
+        else
+        {
+            return Contrincante;
+        }
+
+        }
+        return PersonajePrincipal;
+    }
+    private static int elMasCercano(){
+        var random = new Random();
+        int numeroContrincante;
+        int numeroDelMago;
+        int numeroIngresado;
+        string? buffer;
+        Console.WriteLine("== Ingrese un número del 1 al 10 ==");
+        Console.WriteLine("El que se acerca más al número pensado por el mago");
+        Console.WriteLine("Recibira +10 de fuerza momentaneamente");
+        Console.WriteLine("Ahora. ESCRIBE!!");
+        Console.Write("Ingresa: ");
+        buffer = Console.ReadLine();
+        while ( !(int.TryParse(buffer, out numeroIngresado)) || (numeroIngresado < 1 || numeroIngresado > 10) )
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine("Creo que no me has entendido bien");
+            Console.WriteLine("== Ingrese un número del 1 al 10 ==");
+            Console.Write("Intentelo de vuelta: ");
+            buffer = Console.ReadLine();
+        }
+        numeroContrincante = random.Next(1,11);
+        numeroDelMago = random.Next(1,11);
+        if (distanciaEntreNumeros(numeroDelMago, numeroContrincante) ==  distanciaEntreNumeros(numeroDelMago,numeroIngresado))
+        {
+            return 3;           
+        }else
+        {
+            if (distanciaEntreNumeros(numeroDelMago, numeroContrincante) <  distanciaEntreNumeros(numeroDelMago,numeroIngresado))
+            {
+                return 1;
+            }else
+            {
+                return 2;
+            }
+        }
+    }
+    private static int distanciaEntreNumeros(int numero1, int numero2){
+        int distancia = numero1 - numero2;
+        if (distancia < 0)
+        {
+            distancia *= (-1);
+        }
+        return distancia;
+    }
+    private static Personaje recibirBeneficio(Personaje Vencedor, int opcion){
+        switch (opcion)
+        {
+            case 1:
+            Vencedor.Velocidad += 5;
+            break;
+            case 2:
+            Vencedor.Destreza += 2;
+            break;
+            case 3:
+            Vencedor.Fuerza += 5;
+            break;
+            case 4:
+            Vencedor.Nivel += 5;
+            break;
+            case 5:
+            Vencedor.Defensa += 5;
+            break;
+            case 6:
+            Vencedor.Salud += 20;
+            break;
+            default:
+            Vencedor.Salud += 20;
+            break;
+        }
+        return Vencedor;
     }
 }// juego (secuencia simulada , secuencia jugador)
 // for coutn paso i+=2 p1 = i p2 = i+1
