82afe7509cb7365c2307f925e9f86111064be7a7
NicoPed <nikopedraza10@gmail.com>
2023-06-19 01:10:05 -0300

    Realizado Combate Simulado

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ internal class Program
             listaDePersonajes =AyudaJson.LeerPersonajes(archivoJson);
         }    
             mostrarLista(listaDePersonajes);
-
+            
     }
 
     private static void mostrarLista(List<Personaje> listaDePersonajes){
@@ -47,4 +47,55 @@ internal class Program
         Console.WriteLine("Defensa: "+personaje.Defensa);
         Console.WriteLine("Salud: "+personaje.Salud);
     }
-}
+
+    private static Personaje combateSimulado(Personaje luchador1, Personaje luchador2){
+        int golpeP1;
+        int golpeP2;
+        int moneda;
+        var random = new Random();
+        while (luchador1.Salud >0 && luchador2.Salud >0)
+        {
+            moneda = random.Next(1,3);
+            if (moneda == 1)
+            {
+                golpeP1 = calcularGolpe(luchador1,10);
+                golpeP2 = calcularGolpe(luchador2,0);
+            }else
+            {
+                golpeP1 = calcularGolpe(luchador1,0);
+                golpeP2 = calcularGolpe(luchador2,10);              
+            }
+            luchador2.Salud -= golpeP1;
+            if (luchador2.Salud > 0)
+            {
+                luchador1.Salud -= golpeP2;
+            }
+        }
+        if (luchador1.Salud <= 0 ){
+            return luchador1;
+        }
+        else
+        {
+            return luchador2;
+        }
+
+    }
+    private static int calcularGolpe(Personaje luchador, int fuerzaExtra){
+        var random = new Random();
+        int danioProvocado = 0;
+        int Ataque = luchador.Destreza * luchador.Fuerza + fuerzaExtra * luchador.Nivel;
+        int Efectividad = random.Next(0,101);
+        int Defensa = luchador.Defensa * luchador.Velocidad;
+        int ConstanteDeAjuste = 500; 
+        danioProvocado = ((Ataque * Efectividad)- Defensa)/ConstanteDeAjuste;
+        return danioProvocado;
+    }
+    private static Personaje combateJugador(Personaje luchador1, Personaje luchador2){
+        while (luchador1.Salud >0 && luchador2.Salud >0)
+        {
+            
+        }
+        return luchador1;
+    }
+}// juego (secuencia simulada , secuencia jugador)
+// for coutn paso i+=2 p1 = i p2 = i+1
